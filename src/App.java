4c64dcd5210981d6078580e9a5108711e36c6274
Hustra03 <erik.paulinder@gmail.com>
2023-10-16 09:59:17 +0200

    new file:   bin/App.class 	new file:   bin/Zip$Node.class 	new file:   bin/Zip.class 	new file:   bin/postnummer.csv 	modified:   src/App.java 	new file:   src/Zip.java 	new file:   src/postnummer.csv

--- src/App.java
+++ src/App.java
@@ -1,5 +1,75 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        lookUpBenchmark();
+    }
+
+    public static void lookUpBenchmark() {
+        Zip zip = new Zip("src\\postnummer.csv");
+
+        long t0;
+        long t1;
+        long minimum1 = Long.MAX_VALUE;
+
+        long minimum2 = Long.MAX_VALUE;
+        long minimum3 = Long.MAX_VALUE;
+        long minimum4 = Long.MAX_VALUE;
+        int attempts = 100;
+        int searches = 10;
+        for (int i = 0; i < attempts; i++) {
+
+            t0 = System.nanoTime();
+            for (int j = 0; j < searches; j++) {
+
+                zip.linearSeach("111 15");
+            }
+            t1 = System.nanoTime() - t0;
+            if (t1 < minimum1) {
+                minimum1 = t1;
+            }
+        }
+        System.out.println("Linear Search 111 15 : " + minimum1);
+
+        for (int i = 0; i < attempts; i++) {
+
+            t0 = System.nanoTime();
+            for (int j = 0; j < searches; j++) {
+
+                zip.linearSeach("984 99");
+            }
+            t1 = System.nanoTime() - t0;
+            if (t1 < minimum2) {
+                minimum2 = t1;
+            }
+        }
+        System.out.println("Linear Search 984 99 : " + minimum2);
+
+        for (int i = 0; i < attempts; i++) {
+
+            t0 = System.nanoTime();
+            for (int j = 0; j < searches; j++) {
+
+                zip.binarySearch("111 15");
+            }
+            t1 = System.nanoTime() - t0;
+            if (t1 < minimum3) {
+                minimum3 = t1;
+            }
+        }
+        System.out.println("Binary Search 111 15 : " + minimum3);
+
+        for (int i = 0; i < attempts; i++) {
+
+            t0 = System.nanoTime();
+            for (int j = 0; j < searches; j++) {
+
+                zip.binarySearch("984 99");
+            }
+            t1 = System.nanoTime() - t0;
+            if (t1 < minimum4) {
+                minimum4 = t1;
+            }
+        }
+        System.out.println("Binary Search 984 99 : " + minimum4);
+
     }
 }
---
+++ src/Zip.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Zip {
+    Node[] data;
+    int max;
+
+    public class Node {
+
+        String code;
+        String name;
+        int pop;
+
+        public Node(String code, String name, int pop) {
+            this.code = code;
+            this.name = name;
+            this.pop = pop;
+        }
+
+        public String getCode() {
+            return this.code;
+        }
+
+    }
+
+    public Zip(String file) {
+        data = new Node[10000];
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                data[i++] = new Node(row[0], row[1], Integer.valueOf(row[2]));
+            }
+            max = i - 1;
+        } catch (Exception e) {
+            System.out.println(" file " + file + " not found");
+        }
+    }
+
+    public boolean linearSeach(String zip) {
+        int position = 0;
+        while (zip.equalsIgnoreCase(data[position].getCode()) == false) {
+            position += 1;
+            if (position > max) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean binarySearch(String zip) {
+
+        int currentMin = 0;
+        int currentMax = max;
+        int position = (currentMin + currentMax) / 2;
+        while (true) {
+            if (0 > zip.compareToIgnoreCase(data[position].getCode())) {
+                currentMax = position-1;
+                position = (currentMin + currentMax) / 2;
+            }
+            if (0 == zip.compareToIgnoreCase(data[position].getCode())) {
+                return true;
+            }
+            if (0 < zip.compareToIgnoreCase(data[position].getCode())) {
+                currentMin = position+1;
+                position = (currentMin + currentMax) / 2;
+            }
+            if (currentMin > currentMax) {
+                return false;
+            }
+        }
+
+    }
+
+}
